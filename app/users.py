7516325591f5001c433c81d9d2b28cca28965dc2
adamTrask96 <110453104+adamTrask96@users.noreply.github.com>
2023-02-26 15:02:36 +0500

    Update users.py

--- app/users.py
+++ app/users.py
@@ -28,24 +28,30 @@ def users_search(sex, age_at, age_to, city, status):
     all_persons = {}
     link_profile = 'https://vk.com/id'
     vk = vk_api.VkApi(token=vk_access_token)
-    response = vk.method('users.search', {'sort': 1,
-                                          'sex': sex,
-                                          'status': status,
-                                          'age_from': age_at,
-                                          'age_to': age_to,
-                                          'has_photo': 1,
-                                          'count': 50,
-                                          'fields': 'verified, sex, bdate, city, home_town, has_photo, online, domain, nickname, screen_name, maiden_name, friend_status',
-                                          'city': city})
-    for item in response['items']:
-        if item["is_closed"] == False:
-            for key in item.keys():
-                if key == 'city':
-                    all_persons[item['id']]['city_id'] = item[key]['id']
-                    all_persons[item['id']]['city_title'] = item[key]['title']
-                elif item['id'] not in all_persons.keys():
-                    all_persons[item['id']] = {f'{key}' : item[key]}
-                else:
-                    all_persons[item['id']][key] = item[key]
-            all_persons[item['id']]['link_pro'] = link_profile + str(item['id'])
+    try:
+        response = vk.method('users.search', {'sort': 1,
+                                              'sex': sex,
+                                              'status': status,
+                                              'age_from': age_at,
+                                              'age_to': age_to,
+                                              'has_photo': 1,
+                                              'count': 50,
+                                              'fields': 'verified, sex, bdate, city, home_town, has_photo, online, domain, nickname, screen_name, maiden_name, friend_status',
+                                              'city': city})
+        if 'items' not in response:
+            raise ValueError('Response does not contain "items" key')
+        for item in response['items']:
+            if item["is_closed"] == False:
+                for key in item.keys():
+                    if key == 'city':
+                        all_persons[item['id']]['city_id'] = item[key]['id']
+                        all_persons[item['id']]['city_title'] = item[key]['title']
+                    elif item['id'] not in all_persons.keys():
+                        all_persons[item['id']] = {f'{key}' : item[key]}
+                    else:
+                        all_persons[item['id']][key] = item[key]
+                all_persons[item['id']]['link_pro'] = link_profile + str(item['id'])
+    except Exception as e:
+        print(f'Error occurred while searching for users: {e}')
+        return None
     return all_persons
