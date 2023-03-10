231b4e3778cf9c452866d95e1911960390e423f4
adamTrask96 <110453104+adamTrask96@users.noreply.github.com>
2023-03-10 10:22:44 +0500

    Update photo.py

--- app/photo.py
+++ app/photo.py
@@ -2,23 +2,31 @@ import vk_api
 import pandas as pd
 from config import vk_access_token, access_token_group, group_id
 
-
 def get_top_photos(owner_id):
     '''Returns the top three photos for a user or group, sorted by number of likes.'''
     photos_dict = {}
     vk = vk_api.VkApi(token=vk_access_token)
-    response = vk.method('photos.getAll',
-                          {'owner_id': owner_id,
-                           'extended': 1,
-                           'count': 200})
+    try:
+        response = vk.method('photos.getAll',
+                              {'owner_id': owner_id,
+                               'extended': 1,
+                               'count': 200})
+    except vk_api.ApiError as e:
+        print(f"Error while requesting photos from API: {e}")
+        return None
+    
     for item in response['items']:
-        m = 0
-        for i in item['sizes']:
-            if i['height'] > m:
-                m = i['height']
-                url = i['url']
-        photos_dict[item['id']] = {'url': url,
-                                   'likes': item['likes']['count']}
+        try:
+            m = 0
+            for i in item['sizes']:
+                if i['height'] > m:
+                    m = i['height']
+                    url = i['url']
+            photos_dict[item['id']] = {'url': url,
+                                       'likes': item['likes']['count']}
+        except KeyError as e:
+            print(f"Error while processing photo data: {e}")
+            continue
 
     df = pd.DataFrame(photos_dict)
     data = df.transpose()
@@ -30,6 +38,11 @@ def get_top_photos(owner_id):
 def get_messages_upload_server():
     '''Returns the upload server URL for group messages.'''
     vk = vk_api.VkApi(token=access_token_group)
-    response = vk.method('photos.getMessagesUploadServer',
-                          {'group_id': group_id})
+    try:
+        response = vk.method('photos.getMessagesUploadServer',
+                              {'group_id': group_id})
+    except vk_api.ApiError as e:
+        print(f"Error while requesting upload server from API: {e}")
+        return None
+    
     return response
