530c24440c0d9030a4c39ecb71d0f403455e2e0f
adamTrask96 <110453104+adamTrask96@users.noreply.github.com>
2023-03-10 10:06:00 +0500

    Update users.py

--- app/users.py
+++ app/users.py
@@ -1,6 +1,5 @@
 from config.imports import *
 
-
 def users_get(ids):
     ''' Возвращает расширенную информацию о пользователях.
     '''
@@ -10,17 +9,15 @@ def users_get(ids):
     response = vk.method('users.get', {'user_ids': ids,
                                        'fields': 'verified, sex, bdate, city, home_town, has_photo, online, domain, nickname, screen_name, maiden_name, friend_status'})
     for item in response:
+        all_persons[item['id']] = {'id': item['id']}
         for key in item.keys():
             if key == 'city':
                 all_persons[item['id']]['city_id'] = item[key]['id']
                 all_persons[item['id']]['city_title'] = item[key]['title']
-            elif item['id'] not in all_persons.keys():
-                all_persons[item['id']] = {f'{key}' : item[key]}
             else:
                 all_persons[item['id']][key] = item[key]
         all_persons[item['id']]['link_pro'] = link_profile + str(item['id'])
     return all_persons
-
 
 def users_search(sex, age_at, age_to, city, status):
     ''' Возвращает список пользователей в соответствии с заданным критерием поиска.
@@ -41,13 +38,12 @@ def users_search(sex, age_at, age_to, city, status):
         if 'items' not in response:
             raise ValueError('Response does not contain "items" key')
         for item in response['items']:
-            if item["is_closed"] == False:
+            if item.get("is_closed", False) == False:
+                all_persons[item['id']] = {'id': item['id']}
                 for key in item.keys():
                     if key == 'city':
                         all_persons[item['id']]['city_id'] = item[key]['id']
                         all_persons[item['id']]['city_title'] = item[key]['title']
-                    elif item['id'] not in all_persons.keys():
-                        all_persons[item['id']] = {f'{key}' : item[key]}
                     else:
                         all_persons[item['id']][key] = item[key]
                 all_persons[item['id']]['link_pro'] = link_profile + str(item['id'])
