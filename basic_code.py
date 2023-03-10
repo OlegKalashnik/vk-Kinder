83757d3ce171e3c4460f94177ee2adf6307f6c38
adamTrask96 <110453104+adamTrask96@users.noreply.github.com>
2023-03-10 10:26:03 +0500

    Update basic_code.py

--- basic_code.py
+++ basic_code.py
@@ -1,24 +1,24 @@
 from random import randrange
-
 import vk_api
 from vk_api.longpoll import VkLongPoll, VkEventType
 
 token = input('Token: ')
-
 vk = vk_api.VkApi(token=token)
 longpoll = VkLongPoll(vk)
 
-
 def write_msg(user_id, message):
-    vk.method('messages.send', {'user_id': user_id, 'message': message,  'random_id': randrange(10 ** 7),})
-
+    try:
+        vk.method('messages.send', {'user_id': user_id, 'message': message, 'random_id': randrange(10 ** 7)})
+    except vk_api.exceptions.ApiError as e:
+        print(f"Failed to send message: {e}")
 
 for event in longpoll.listen():
     if event.type == VkEventType.MESSAGE_NEW:
-
         if event.to_me:
-            request = event.text
-
+            try:
+                request = event.text
+            except AttributeError:
+                continue
             if request == "привет":
                 write_msg(event.user_id, f"Хай, {event.user_id}")
             elif request == "пока":
