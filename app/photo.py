e94d2532cd7b647c8022aa943e2941a6a6f984a1
adamTrask96 <110453104+adamTrask96@users.noreply.github.com>
2023-03-10 09:57:23 +0500

    Update photo.py

--- app/photo.py
+++ app/photo.py
@@ -1,12 +1,11 @@
-from config.imports import *
+import vk_api
+import pandas as pd
+from config import vk_access_token, access_token_group, group_id
 
 
-def photos_getAll(owner_id):
-    ''' Возвращает все фотографии пользователя или сообщества в антихронологическом порядке.
-    Проводит сортировку по количеству лайков и отбирает три с наибольшими значениями.
-    '''
-    dict = {}
-    link_profile = 'https://vk.com/id'
+def get_top_photos(owner_id):
+    '''Returns the top three photos for a user or group, sorted by number of likes.'''
+    photos_dict = {}
     vk = vk_api.VkApi(token=vk_access_token)
     response = vk.method('photos.getAll',
                           {'owner_id': owner_id,
@@ -18,23 +17,18 @@ def photos_getAll(owner_id):
             if i['height'] > m:
                 m = i['height']
                 url = i['url']
-        dict[item['id']] = {'url' : url,
-                            'likes' : item['likes']['count']}
+        photos_dict[item['id']] = {'url': url,
+                                   'likes': item['likes']['count']}
 
-    df = pd.DataFrame(dict)
+    df = pd.DataFrame(photos_dict)
     data = df.transpose()
     data = data.sort_values(by='likes', ascending=False)
-    data1 = data.head(3)
-    dict2 = data1.to_dict('index')
-    return dict2
+    top_photos = data.head(3)
+    return top_photos.to_dict('index')
 
 
-def photos_getMessagesUploadServer():
-    ''' Возвращает все фотографии пользователя или сообщества в антихронологическом порядке.
-    Проводит сортировку по количеству лайков и отбирает три с наибольшими значениями.
-    '''
-    dict = {}
-    link_profile = 'https://vk.com/id'
+def get_messages_upload_server():
+    '''Returns the upload server URL for group messages.'''
     vk = vk_api.VkApi(token=access_token_group)
     response = vk.method('photos.getMessagesUploadServer',
                           {'group_id': group_id})
